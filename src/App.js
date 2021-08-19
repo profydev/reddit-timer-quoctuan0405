c132fdcfde387cb7e57202547537605228dcc156
Your Name <you@example.com>
2021-08-19 18:04:15 +0700

    add header component with its placeholder text

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={() => <>/ route</>} />
         <Route exact path="/search" component={() => <>/search route</>} />
