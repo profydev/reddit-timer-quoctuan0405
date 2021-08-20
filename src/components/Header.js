3388e3536599beb80f824dc4aea1b6f7f0b845b3
Your Name <you@example.com>
2021-08-20 10:49:41 +0700

    Add and position logo to the left and links to the right but without correct padding and margin

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import styles from './Header.module.css';
 
 function Header() {
   return (
-    <header>
-      Header Placeholder
+    <header className={styles.header}>
+      <picture>
+        <img src={logo} alt="reddit timer logo" />
+      </picture>
+      <ul className={styles.links}>
+        <li><Link to="/search?query=javascript">Search</Link></li>
+        <li><Link to="/#how-it-works">How it works</Link></li>
+        <li><Link to="/#about">About</Link></li>
+      </ul>
     </header>
   );
 }
