b82adbc6360b1f166abe2c558db34fa57d6710bf
Your Name <you@example.com>
2021-08-20 21:01:32 +0700

    implement pixel perfect layout in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,18 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <header className={styles.header}>
-      <picture>
-        <img src={logo} alt="reddit timer logo" />
-      </picture>
-      <ul className={styles.links}>
-        <li><Link to="/search?query=javascript">Search</Link></li>
-        <li><Link to="/#how-it-works">How it works</Link></li>
-        <li><Link to="/#about">About</Link></li>
-      </ul>
+      <Link to="/" className={styles.logo}>
+        <picture className={styles.logo}>
+          <img src={logo} alt="reddit timer logo" />
+        </picture>
+      </Link>
+      <nav className={styles.nav}>
+        <ul>
+          <li><Link to="/search?query=javascript">Search</Link></li>
+          <li><Link to="/#how-it-works">How it works</Link></li>
+          <li><Link to="/#about">About</Link></li>
+        </ul>
+      </nav>
     </header>
   );
 }
