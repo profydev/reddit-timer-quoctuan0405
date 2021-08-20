852d06c4e6b5d6fb1a8645a05175fc9d308885a0
Your Name <you@example.com>
2021-08-20 10:56:38 +0700

    rename App.css to GlobalStyle.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css';
-import './App.css';
+import './GlobalStyle.css';
 
 function App() {
   return (
